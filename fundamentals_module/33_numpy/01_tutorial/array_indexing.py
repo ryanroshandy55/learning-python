f1ee222c132fa46e6a3aacecdee489b3e96b5429
ryanroshandy55 <ryanroshandy7@gmail.com>
2024-11-23 21:20:04 +0700

    update

--- fundamentals_module/33_numpy/01_tutorial/array_indexing.py
+++ fundamentals_module/33_numpy/01_tutorial/array_indexing.py
@@ -9,4 +9,12 @@ arr = np.array([1, 2, 3, 4])
 
 print(arr[0])
 print(arr[1])
-print(arr[2])
+print(arr[2] + arr[3])
+
+print("------------------")
+
+arr = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+
+print(arr)
+
+print('2nd element on 1st row: ', arr[0, 1])
