c579ebf89a6b48ae628021ff3e49f596250f30a6
ryanroshandy55 <ryanroshandy7@gmail.com>
2024-11-25 13:09:10 +0700

    splitting searching and sorting array with numpy

---
+++ fundamentals_module/33_numpy/01_tutorial/12_splitting_array.py
@@ -0,0 +1,61 @@
+# Splitting NumPy Arrays
+# Splitting is reverse operation of Joining.
+# Joining merges multiple arrays into one and Splitting breaks one array into multiple.
+# We use array_split() for splitting arrays, we pass it the array we want to split and the number of splits.
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6])
+
+newarr = np.array_split(arr, 3)
+
+print(newarr)
+
+# If the array has less elements than required, it will adjust from the end accordingly
+# Raise an error
+# newarr1 = np.array(arr, 4)
+
+# print(newarr1)  # Raise an error
+
+# Access the splitted array
+arr = np.array([1, 2, 3, 4, 5, 6])
+
+newarr = np.array_split(arr, 3)
+
+print(newarr[0])
+print(newarr[1])
+print(newarr[2])
+
+# Example
+# Split the 2-D array into three 2-D arrays
+arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+
+newarr = np.array_split(arr, 3)
+
+print(newarr)
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [
+               10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+newarr = np.array_split(arr, 3)
+
+print(newarr)
+
+# Example
+# Split the 2-D array into three 2-D arrays along rows.
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [
+               10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+newarr = np.array_split(arr, 3, axis=1)
+
+print(newarr)
+
+
+# Example
+# Use the hsplit() method to split the 2-D array into three 2-D arrays along rows.
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [
+               10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+newarr = np.hsplit(arr, 3)
+
+print(newarr)
---
+++ fundamentals_module/33_numpy/01_tutorial/14_sorting_array.py
@@ -0,0 +1,13 @@
+# Sorting Arrays
+# Sorting means putting elements in an ordered sequence.
+# Ordered sequence is any sequence that has an order corresponding to elements, like numeric or alphabetical, ascending or descending.
+# The NumPy ndarray object has a function called sort(), that will sort a specified array.
+
+# Example
+# Sort the array:
+
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+
+print(np.sort(arr))
